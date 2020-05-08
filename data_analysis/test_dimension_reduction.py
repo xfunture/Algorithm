9613b5ab84b962a27827164e2c3f3eeab6f373fd
xfunture <15768184216@163.com>
2020-05-08 17:16:01 +0800

    compare SNE,PCA,kmeans

--- data_analysis/test_dimension_reduction.py
+++ data_analysis/test_dimension_reduction.py
@@ -29,32 +29,34 @@ def main():
 	train_data,train_label,n_samples,n_features = load_data()
 	train_data = normalize(train_data)
 	print(train_label.shape)
+	# # fig = plt.figure(figsize=(20,10))
+	fig = plt.figure()
 	
-	tsne = TSNE(n_components=3,random_state=42,perplexity=20,n_iter=5000)
+	
+	tsne = TSNE(n_components=3,random_state=42,perplexity=10,n_iter=5000)
 	result1 = tsne.fit_transform(X=train_data)
 	print(result1.shape)
-	# fig = plt.figure(figsize=(20,10))
-	fig = plt.figure()
-	ax1 = fig.add_subplot(111, projection='3d')
-	# ax = Axes3D(fig)
-	for i in range(result1.shape[0]):
-		if train_label[i] == 0:
-			ax1.scatter(result1[i,0],result1[i,1],result1[i,2],c='r')
-		elif train_label[i] == 1:
-			ax1.scatter(result1[i,0],result1[i,1],result1[i,2],c='b')
-		else:
-			ax1.scatter(result1[i,0],result1[i,1],result1[i,2],c='y')
-	ax1.set_xlabel('X label')
-	ax1.set_ylabel('Y label')
-	ax1.set_zlabel('Z label')
-	ax1.set_title('SNE')
+
+	# ax1 = fig.add_subplot(111, projection='3d')
+	# # ax = Axes3D(fig)
+	# for i in range(result1.shape[0]):
+	# 	if train_label[i] == 0:
+	# 		ax1.scatter(result1[i,0],result1[i,1],result1[i,2],c='r')
+	# 	elif train_label[i] == 1:
+	# 		ax1.scatter(result1[i,0],result1[i,1],result1[i,2],c='b')
+	# 	else:
+	# 		ax1.scatter(result1[i,0],result1[i,1],result1[i,2],c='y')
+	# ax1.set_xlabel('X label')
+	# ax1.set_ylabel('Y label')
+	# ax1.set_zlabel('Z label')
+	# ax1.set_title('SNE')
 	#
 	# pca = PCA(n_components=3, random_state=42)
 	# result2 = pca.fit_transform(X=train_data)
 	# print(result2.shape)
-	# ax2 = fig.add_subplot(212, projection='3d')
+	# ax2 = fig.add_subplot(111, projection='3d')
 	# # ax = Axes3D(fig)
-	# for i in range(result1.shape[0]):
+	# for i in range(result2.shape[0]):
 	# 	if train_label[i] == 0:
 	# 		ax2.scatter(result2[i, 0], result2[i, 1], result2[i, 2], c='r')
 	# 	elif train_label[i] == 1:
@@ -65,22 +67,22 @@ def main():
 	# ax2.set_xlabel('X label')
 	# ax2.set_ylabel('Y label')
 	# ax2.set_zlabel('Z label')
-	#
-	# km = KMeans(n_clusters=3,random_state=42)
-	# result = km.fit_transform(X=train_data)
-	# ax = fig.add_subplot(111, projection='3d')
-	# # ax = Axes3D(fig)
-	# for i in range(result1.shape[0]):
-	# 	if train_label[i] == 0:
-	# 		ax.scatter(result[i, 0], result[i, 1], result[i, 2], c='r')
-	# 	elif train_label[i] == 1:
-	# 		ax.scatter(result[i, 0], result[i, 1], result[i, 2], c='b')
-	# 	else:
-	# 		ax.scatter(result[i, 0], result[i, 1], result[i, 2], c='y')
-	# ax.set_title('Kmeans')
-	# ax.set_xlabel('X label')
-	# ax.set_ylabel('Y label')
-	# ax.set_zlabel('Z label')
+	# #
+	km = KMeans(n_clusters=3,random_state=42)
+	result = km.fit_transform(X=train_data)
+	ax = fig.add_subplot(111, projection='3d')
+	# ax = Axes3D(fig)
+	for i in range(result1.shape[0]):
+		if train_label[i] == 0:
+			ax.scatter(result[i, 0], result[i, 1], result[i, 2], c='r')
+		elif train_label[i] == 1:
+			ax.scatter(result[i, 0], result[i, 1], result[i, 2], c='b')
+		else:
+			ax.scatter(result[i, 0], result[i, 1], result[i, 2], c='y')
+	ax.set_title('Kmeans')
+	ax.set_xlabel('X label')
+	ax.set_ylabel('Y label')
+	ax.set_zlabel('Z label')
 	plt.show()
 
 main()
